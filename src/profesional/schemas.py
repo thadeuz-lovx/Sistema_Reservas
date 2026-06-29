604ffc9541c8380777516e72bc7d40ada38e7395
thadeuz-lovx <lovxgr@gmail.com>
2026-06-29 14:03:06 -0400

    Creacion de la base Schema de profesionales

---
+++ src/profesional/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class ProfessionalBase(BaseModelse):
+    nombre: str
+    apellido: str
+    especialidad: str
+    codigo: str
+    estado: str
+
+class ProfessionalMake(ProfessionalBase):
+    pass
+
+class ProfessionalUptade(BaseModel):
+    nombre: Optional[str] = None
+    apellido: Optional[str] = None
+    especialidad: Optional[str] = None
+    codigo: Optional[str] = None
+    estado: Optional[str] = None
+
+class ProfessionaResponse(ProfessionalBase):
+    id: int
+
+    class Config:
+        from_attributes = True
